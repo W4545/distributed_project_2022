85eb03ccdd86f11d1975260b7ef196cdafb677c9
aaraujo97 <51144667+aaraujo97@users.noreply.github.com>
2022-04-22 16:35:10 -0400

    Added functionality for listening clients
    -Commented out old logging variables/functions
    -Still needs testing

--- TCPClient/TCPClient.java
+++ TCPClient/TCPClient.java
@@ -145,8 +145,8 @@ public class TCPClient {
         logOn(myID, SockNum, out); //begin logon process
         if (logOnStatus(in) == true) //if IDGOOD
         {
-            PrintWriter clientOut;
-            BufferedReader clientIn;
+            PrintWriter clientOut = null;
+            BufferedReader clientIn = null;
 
             if (config.getProperty("status").toString() != "listening") //if client is requesting
             {
@@ -184,23 +184,19 @@ public class TCPClient {
                 String fromClient; //messages sent from other client
                 String fromThis; // messages sent to Client
 
-                long t0, t1, t;
 
                 int sendCount = 0;
 
-                // Communication while loop
+                // Communication while loop for sending files to a client
                 while ((fromClient = clientIn.readLine()) != null) {
                     System.out.println("Other Client said: " + fromClient);
-                    t1 = System.currentTimeMillis();
 
-                    t = t1 - t0;
-
-                    log(t, fromClient.getBytes().length, "OTHER_CLIENT_MESSAGE");
+                    //log(t, fromClient.getBytes().length, "OTHER_CLIENT_MESSAGE");
 
                     if (fromClient.equals("Bye.")) // exit statement
                         break;
 
-                    System.out.println("Cycle time: " + t);
+                    //System.out.println("Cycle time: " + t);
 
                     if (sendCount > 5) {
                         System.out.println("Send count reached");
@@ -227,14 +223,100 @@ public class TCPClient {
                     }
 
                     fileInputStream.close();
-                    t0 = System.currentTimeMillis();
+                    //t0 = System.currentTimeMillis();
                 }
-            } else {
-                out.write("GOODBYE");
-                System.out.println("Client ended communication: IDBAD");
+                //close connections
+                Socket.close();
+            } else { //else this client is listening
+
+                //Accepting connections
+                ServerSocket serverSocket = null; // server socket for accepting connections
+                Socket clientSocket = null;
+                boolean running = true;
+
+                try {
+                    serverSocket = new ServerSocket(SockNum);
+                    System.out.println("This Client is Listening on port: " + String.valueOf(SockNum));
+                } catch (IOException e) {
+                    System.err.println("Could not listen on port: " + String.valueOf(SockNum));
+                    System.exit(1);
+                }
+                String fromClient;
+                String fromMe;
+                int transferCount = 0;
+
+                // Creating threads with accepted connections
+                while (running) {
+                    try {
+                        clientSocket = serverSocket.accept();
+
+                        // Communication while loop
+                        while ((fromClient = clientIn.readLine()) != null) {
+                            System.out.println("Client-" +destinationID+ " said: " + fromClient);
+                           // long t1 = System.currentTimeMillis();
+                            //long t = t1 - t0;
+
+                            //log(t, fromClient.getBytes().length, "SERVER_MESSAGE");
+
+                            if (fromClient.contains("STARTFILE")) {
+                                transferCount += 1;
+                                String filePath = dialog.getFiles()[0].getParentFile() + "\\" + fromClient.substring(fromClient.indexOf(' '));
+                                System.out.println(filePath);
+                                File file = new File(filePath);
+
+                                if (file.exists())
+                                    file.delete();
+                                FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+
+                                DataInputStream dataInputStream = new DataInputStream(Socket.getInputStream());
+                                long fileSize = dataInputStream.readLong();
+
+                                byte[] buffer = new byte[8 * 1024];
+                                int dataReceived = 0;
+                                //t0 = System.currentTimeMillis();
+                                while (fileSize > 0 && (dataReceived = dataInputStream.read(buffer, 0, (int) Math.min(buffer.length, fileSize))) != -1) {
+                                   // t1 = System.currentTimeMillis();
+
+                                   // t = t1 - t0;
+                                   // log(t, dataReceived, "FILE_TRANSFER_CHUNK");
+                                    fileOutputStream.write(buffer, 0, dataReceived);
+                                    fileSize -= dataReceived;
+                                   // t0 = System.currentTimeMillis();
+                                }
+
+                                fileOutputStream.close();
+                                out.println("Transfer Complete.");
+                            } else {
+
+                                fromMe = fromClient.toUpperCase(); // converting received message to upper case
+                                System.out.println("This Client said: " + fromMe);
+
+                                if (fromClient.equals("Bye.")) // exit statement
+                                    break;
+
+
+                                clientOut.println(fromClient); // sending the converted message back to the Client via ServerRouter
+                            }
+
+                            //t0 = System.currentTimeMillis();
+                            //communication loop ended, end outer while loop
+                            running = false;
+                        }
+
+                        //close connections once communication is finished
+                        clientSocket.close();
+                        serverSocket.close();
+                    }
+                    catch (IOException e) {
+                        System.err.println("Client/Client failed to connect.");
+                        System.exit(1);
+                        running = false;
+                    }
+                }//end while
+
             }
         }
-        
+
 
     }
 
